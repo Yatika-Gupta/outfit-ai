3df3504b9384e376bdd6805c23e5da67fcc938af
Yatika-Gupta <yatikagupta18@gmail.com>
2026-05-25 19:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-# Load model
-model = load_model("outfit_model.h5")
+st.title("👗 Outfit AI Classifier")
 
 class_names = ['STREET', 'casual']
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).resize((224, 224))
 
     st.image(img, caption="Uploaded Image", use_container_width=True)
 
-    # dummy prediction (temporary)
     result = "casual"
 
     st.success(f"Predicted Outfit: {result}")
